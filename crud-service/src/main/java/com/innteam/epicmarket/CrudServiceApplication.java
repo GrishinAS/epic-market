d64badf63321abcb75412b184d85a0b1ac9efc57
GrishinAS <9505467155@mail.ru>
2022-05-11 17:24:04 +0500

    Kubernetes support (#2)
    * Kubernetes support

--- crud-service/src/main/java/com/innteam/epicmarket/CrudServiceApplication.java
+++ crud-service/src/main/java/com/innteam/epicmarket/CrudServiceApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class CrudServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CrudServiceApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(CrudServiceApplication.class, args);
+  }
 }
